bca8125ef091081d31a072bef43debf5bbce212e
史素佳 <1303901763@qq.com>
2018-11-26 14:50:18 +0800

    Add files via upload
    UPUP组项目

---
+++ wordpress/src/ludanqi/Demo.java
@@ -0,0 +1,7 @@
+package ludanqi;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
---
+++ wordpress/src/sehnmengqi/Demo.java
@@ -0,0 +1,7 @@
+package sehnmengqi;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
