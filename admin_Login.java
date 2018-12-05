3fd12ce179e832f40ef8e9fd5fdad9a7fc5a9221
yuezhewei <1084170857@qq.com>
2018-12-05 21:20:16 +0800

    第二
    第二

---
+++ admin_Login.java
@@ -0,0 +1,37 @@
+package com.webtest.demo;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.webtest.core.BaseTest;
+/**
+ * author:lihuanzhen
+ * Settings中常规的测试
+ */
+public class admin_Login extends BaseTest {
+	@BeforeMethod
+   public void testLogin() {
+	   //打开页面
+	   webtest.open("http://localhost:8032/wordpress/wp-login.php?loggedout=true");
+	   //文本框输入
+	   webtest.type("name=log","admin");
+	   webtest.type("name=pwd","123456");
+	   webtest.click("xpath=//input[@type='submit']");
+	   assertTrue(webtest.isTextPresent("登录"));	   
+   }
+   @Test(priority=1)
+   public void routine() throws InterruptedException {
+	   webtest.click("link=Settings");
+	   webtest.typeAndClear("id=blogname","upup");	   
+	   webtest.typeAndClear("name=blogdescription", "wordpress站点");
+	   webtest.typeAndClear("name=new_admin_email", "541520730@qq.com");
+	   webtest.click("xpath=//input[@type='checkbox']");
+	   webtest.selectByValue("id=default_role","editor");
+	   webtest.selectByValue("id=WPLANG", "zh_CN");
+	   webtest.click("xpath=//input[@id='submit']");
+	   
+   } 
+}
+
