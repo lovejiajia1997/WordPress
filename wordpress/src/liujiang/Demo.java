ccf78f438d089db8ad582334a4bb0cb23a54eec6
lovejiajia1997 <1303901763@qq.com>
2018-12-26 10:24:49 +0800

    更新

--- wordpress/src/liujiang/Demo.java
+++
@@ -1,7 +0,0 @@
-package liujiang;
-
-public class Demo {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
-}
--- wordpress/src/yuezhewei/Demo.java
+++
@@ -1,7 +0,0 @@
-package yuezhewei;
-
-public class Demo {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
-}
